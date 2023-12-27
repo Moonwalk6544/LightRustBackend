e090d667c6eb0139d2e60ecbf86f90a5c6c2d2d9
Moonwalk6544 <154779263+moonwalk6544@users.noreply.github.com>
2023-12-26 18:19:11 -0700

    Initial start for project :D

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+
+use actix_web::{Responder, get, patch, post, HttpResponse, HttpServer, App};
+
+use std::io;
+
+
+
+
+
+
+#[get("/")]
+async fn get_main() -> impl Responder {
+    HttpResponse::Ok().body("hello!")
+
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(get_main))
+        .bind("127.0.0.1:8080")?.run()
+        .await
+
+}
